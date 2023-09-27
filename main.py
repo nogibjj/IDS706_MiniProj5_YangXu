da28c46dfc7cba8adba5439e31f80e5127ec60ad
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-09-26 23:42:19 -0400

    fix error

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import psycopg2
-import os
 from config import get_db_config
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 def create_connection():
     config = get_db_config()
     connection = psycopg2.connect(**config)
     return connection
 
+
 def create_table():
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
             CREATE TABLE IF NOT EXISTS items(
                 id SERIAL PRIMARY KEY,
                 date DATE,
@@ -21,14 +23,18 @@ def create_table():
                 price DECIMAL(10, 2),
                 quantity INTEGER
             );
-            """)
+            """
+            )
+
 
 def update_item_price(product, new_price):
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("UPDATE items SET price = %s WHERE product = %s",
-                           (new_price, product))
+            cursor.execute(
+                "UPDATE items SET price = %s WHERE product = %s", (new_price, product)
+            )
             conn.commit()
+
 
 def delete_item(product):
     with create_connection() as conn:
@@ -36,18 +42,24 @@ def delete_item(product):
             cursor.execute("DELETE FROM items WHERE product = %s", (product,))
             conn.commit()
 
+
 def get_items_by_product(product):
     with create_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT * FROM items WHERE product = %s", (product,))
             return cursor.fetchall()
 
+
 def insert_item(date, product, price, quantity):
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO items (date, product, price, quantity) VALUES (%s, %s, %s, %s)",
-                           (date, product, price, quantity))
+            cursor.execute(
+                """INSERT INTO items (date, product, price, quantity)
+                VALUES (%s, %s, %s, %s)""",
+                (date, product, price, quantity),
+            )
             conn.commit()
+
 
 def get_all_items():
     with create_connection() as conn:
@@ -55,17 +67,18 @@ def get_all_items():
             cursor.execute("SELECT * FROM items;")
             return cursor.fetchall()
 
+
 if __name__ == "__main__":
     create_table()
     # Create - Insert data
-    insert_item('2023-09-04', 'TestItem', 1.5, 30)
+    insert_item("2023-09-04", "TestItem", 1.5, 30)
     # Read - Fetch data
     items = get_all_items()
     print(items)
     # Update - Change the price of TestItem to 2.0
-    update_item_price('TestItem', 2.0)
+    update_item_price("TestItem", 2.0)
     # Check the item after the update
-    test_items = get_items_by_product('TestItem')
+    test_items = get_items_by_product("TestItem")
     print(test_items)
     # Delete - Remove TestItem
-    delete_item('TestItem')
+    delete_item("TestItem")
--- test_main.py
+++ test_main.py
@@ -1,26 +1,33 @@
-import pytest
-from main import create_table, insert_item, get_all_items
+from main import (
+    create_table,
+    insert_item,
+    get_all_items,
+    update_item_price,
+    get_items_by_product,
+    delete_item,
+)
+
 
 def test_crud_operations():
     # Create table
     create_table()
 
     # Insert data (Create)
-    insert_item('2023-09-04', 'TestItem', 1.5, 30)
+    insert_item("2023-09-04", "TestItem", 1.5, 30)
 
     # Fetch data (Read)
     items = get_all_items()
     last_item = items[-1]
-    assert last_item[1] == '2023-09-04'
-    assert last_item[2] == 'TestItem'
+    assert last_item[1] == "2023-09-04"
+    assert last_item[2] == "TestItem"
     assert last_item[3] == 1.5
     assert last_item[4] == 30
 
     # Update the price for TestItem (Update)
-    update_item_price('TestItem', 2.0)
-    updated_items = get_items_by_product('TestItem')
+    update_item_price("TestItem", 2.0)
+    updated_items = get_items_by_product("TestItem")
     assert updated_items[0][3] == 2.0
 
     # Remove TestItem (Delete)
-    delete_item('TestItem')
-    assert not get_items_by_product('TestItem')
+    delete_item("TestItem")
+    assert not get_items_by_product("TestItem")
