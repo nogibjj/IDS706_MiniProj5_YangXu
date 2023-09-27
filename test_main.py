019e204b4d8de4a61b921593f54da7172c7d9189
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-09-27 16:27:54 -0400

    black form

--- test_main.py
+++ test_main.py
@@ -2,9 +2,9 @@ import unittest
 from unittest.mock import MagicMock, patch
 from main import create_table, insert_item, get_all_items
 
-class TestMain(unittest.TestCase):
 
-    @patch('main.create_connection')
+class TestMain(unittest.TestCase):
+    @patch("main.create_connection")
     def test_crud_operations(self, mock_create_connection):
         # Mocking the database connection and cursor
         mock_conn = MagicMock()
@@ -19,25 +19,30 @@ class TestMain(unittest.TestCase):
         create_table()
 
         # Insert data (Create)
-        insert_item('2023-09-04', 'TestItem', 1.5, 30)
+        insert_item("2023-09-04", "TestItem", 1.5, 30)
 
         # Fetch data (Read)
-        mock_cursor.fetchall.return_value = [(1, '2023-09-04', 'TestItem', 1.5, 30)]
+        mock_cursor.fetchall.return_value = [(1, "2023-09-04", "TestItem", 1.5, 30)]
         items = get_all_items()
         last_item = items[-1]
-        self.assertEqual(last_item[1], '2023-09-04')
-        self.assertEqual(last_item[2], 'TestItem')
+        self.assertEqual(last_item[1], "2023-09-04")
+        self.assertEqual(last_item[2], "TestItem")
         self.assertEqual(last_item[3], 1.5)
         self.assertEqual(last_item[4], 30)
 
         # Update the price for TestItem (Update)
-        mock_cursor.fetchall.return_value = [(1, '2023-09-04', 'TestItem', 2.0, 30)]
-        updated_items = get_all_items()  # Assuming you want to mock the same method here
+        mock_cursor.fetchall.return_value = [(1, "2023-09-04", "TestItem", 2.0, 30)]
+        updated_items = (
+            get_all_items()
+        )  # Assuming you want to mock the same method here
         self.assertEqual(updated_items[0][3], 2.0)
 
         # Remove TestItem (Delete)
         mock_cursor.fetchall.return_value = []
-        self.assertFalse(get_all_items())  # Assuming you want to mock the same method here
+        self.assertFalse(
+            get_all_items()
+        )  # Assuming you want to mock the same method here
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
